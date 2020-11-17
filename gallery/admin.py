87b2bf47a9d11a84dcf53aca3492dfd0636617ce
mikengugy <mikengugy@gmail.com>
2020-11-17 12:14:29 +0300

    added class photo and ,made changes in the admin file

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Category,Image,Location
+from .models import Photo
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Image)
 admin.site.register(Location)
+admin.site.register(Photo)
--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+class Photo(models.Model):
+   image = CloudinaryField('image')
 
 # Create your models here.
 class Category(models.Model):
